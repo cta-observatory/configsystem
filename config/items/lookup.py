d6ea093425b5ed7a272e292b7731efde1a775388
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-01-29 16:16:17 +0100

    Implement hierarchical lookup item

---
+++ config/items/lookup.py
@@ -0,0 +1,123 @@
+try:
+    # performance improvement for python >= 3.9
+    from functools import cache
+except ImportError:
+    from functools import lru_cache
+    def cache(f):
+        return lru_cache(maxsize=None)(f)
+
+from ..item import Item
+from ..exceptions import ConfigError
+
+
+class HierarchicalLookup:
+
+    def __init__(self, item, hierarchy, default=None, configuration=None):
+        self.hierarchy = tuple(hierarchy)
+        self._indexed_hierarchy = list(enumerate(hierarchy))[::-1]
+        self._expected = '(' + ', '.join(f'<{key} value>' for key in self.hierarchy) + ')'
+
+        self.item = item
+        # allow overriding the default of the item
+        self.default = default if default is not None else self.item.get_default()
+
+        self.configuration = []
+
+        if configuration is None:
+            return
+
+        for lookup_config in configuration:
+            lookup_config = tuple(lookup_config)
+
+            if len(lookup_config) != 3:
+                raise ValueError(f'Lookup definition must be (key, value of key, value), got {lookup_config}')
+
+            key, key_value, value = lookup_config
+            if key not in self.hierarchy:
+                raise ValueError(f'Key {key} not in hierarchy: {self.hierarchy}')
+
+
+            value = item.validate(value)
+            self.configuration.append((key, key_value, value))
+
+    @cache
+    def __getitem__(self, lookup):
+        # support a single value for len(hierarchy) == 1
+        if not isinstance(lookup, tuple):
+            lookup = (lookup, )
+
+        if len(lookup) != len(self.hierarchy):
+            raise IndexError(f"Lookup must be a tuple of form {self._expected}")
+
+        for index, key in self._indexed_hierarchy:
+
+            for (lookup_key, key_value, value) in self.configuration:
+                if key != lookup_key:
+                    continue
+
+                if key_value == lookup[index]:
+                    return value
+
+        return self.default
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(hierarchy={self.hierarchy}, item={self.item})'
+
+
+class Lookup(Item):
+    def __init__(self, item, hierarchy, default_config=None, **kwargs):
+        super().__init__(**kwargs)
+        self.item = item
+        self.hierarchy = hierarchy
+        self.default_config = default_config
+
+
+    def from_config(self, config):
+        try:
+            return HierarchicalLookup(
+                item=self.item,
+                hierarchy=self.hierarchy,
+                **config
+            )
+        except:
+            raise ConfigError(self, config, 'is an invalid config')
+
+
+    def get_default(self):
+        if self.default_config is None:
+            return HierarchicalLookup(self.item, self.hierarchy)
+        return self.from_config(self.default_config)
+
+    def get_default_config(self):
+        if self.default_config is None:
+            return {}
+        return self.default_config
+
+    def validate(self, value):
+        if not isinstance(value, HierarchicalLookup):
+            # see if it's a single value matching our item
+
+            try:
+                value = self.item.validate(value)
+            except ConfigError:
+                raise ConfigError(self, value, f'Single value must be valid for {self.item}')
+
+            return HierarchicalLookup(item=self.item, hierarchy=self.hierarchy, default=value)
+
+        if value.hierarchy != self.hierarchy:
+            raise ConfigError(
+                self, 
+                value,
+                'HierarchicalLookup must have same hierarchy.'
+                f' Expected {self.hierarchy}, got {value.hierarchy}'
+            )
+
+        if type(value.item) is not type(self.item):
+            raise ConfigError(
+                self, 
+                value,
+                'HierarchicalLookup must have same item type.'
+                f' Expected {type(self.item)}, got {type(value)}'
+            )
+
+        return value
---
+++ config/items/tests/test_lookup.py
@@ -0,0 +1,176 @@
+import pytest
+
+
+def test_single_key():
+    from config import Int
+    from config.items.lookup import HierarchicalLookup
+
+    lookup = HierarchicalLookup(
+        item=Int(1),
+        hierarchy=("type", ),
+        configuration=[
+            ("type", "LST", 2),
+            ("type", "MST", 3),
+        ],
+    )
+
+    assert lookup["LST"] == 2
+    assert lookup["MST"] == 3
+
+    # not matching anything -> default
+    assert lookup["SST"] == 1
+
+
+def test_two_keys():
+    from config.items.lookup import HierarchicalLookup
+    from config import Int
+
+    lookup = HierarchicalLookup(
+        item=Int(1),
+        hierarchy=("type", "id"),
+        configuration=[
+            ("type", "LST", 2),
+            ("type", "MST", 3),
+            ("id", 5, 4),
+            ("id", 30, 5),
+        ],
+    )
+
+    assert lookup["LST", 1] == 2
+    assert lookup["MST", 2] == 3
+
+    # id more important then type
+    assert lookup["LST", 5] == 4
+
+    # unknown type, but matches id
+    assert lookup["SST", 30] == 5
+
+    # not matching anything -> default
+    assert lookup["SST", 3] == 1
+
+
+def test_lookup_invalid():
+    from config.items.lookup import HierarchicalLookup
+    from config import Int
+
+    # invalid key in configuration
+    with pytest.raises(ValueError):
+        HierarchicalLookup(
+            item=Int(1),
+            hierarchy=("type", "id"),
+            configuration=[
+                ("type", "LST", 2),
+                ("type", "MST", 3),
+                ("invalid", 5, 4),
+            ],
+        )
+
+    # invalid key in configuration
+    with pytest.raises(ValueError):
+        HierarchicalLookup(
+            item=Int(1),
+            hierarchy=("type", "id"),
+            configuration=[
+                ("type", "MST", 3),
+                ("type", 2),
+            ],
+        )
+
+    # invalid value in configuration
+    with pytest.raises(ValueError):
+        HierarchicalLookup(
+            item=Int(1),
+            hierarchy=("type", "id"),
+            configuration=[
+                ("type", "MST", 3),
+                ("type", "LST", "foo"),
+            ],
+        )
+
+
+    lookup = HierarchicalLookup(
+        item=Int(1),
+        hierarchy=("type", "id"),
+        configuration=[
+            ("type", "MST", 3),
+            ("type", "LST", 2),
+            ("id", 1, 2),
+        ],
+    )
+    with pytest.raises(IndexError):
+        # to few indices
+        lookup[1]
+
+    with pytest.raises(IndexError):
+        # too many indices
+        lookup[1, 2, 3]
+
+
+def test_defaults():
+    from config.items.lookup import HierarchicalLookup
+    from config import Int
+
+    lookup = HierarchicalLookup(Int(), ('foo', 'bar'))
+    assert lookup[1, 2] is None
+
+    lookup = HierarchicalLookup(Int(5), ('foo', 'bar'))
+    assert lookup[1, 2] is 5
+
+
+def test_item():
+    from config import Configurable, Float
+    from config.items.lookup import Lookup, HierarchicalLookup
+
+
+    class Cleaning(Configurable):
+        level = Lookup(Float(5.0), hierarchy=('type', 'id'))
+
+    cleaning = Cleaning()
+    assert isinstance(cleaning.level, HierarchicalLookup)
+    assert cleaning.level['LST', 1] == 5.0
+
+    # override default with a single value
+    cleaning = Cleaning(level=10.0)
+    assert isinstance(cleaning.level, HierarchicalLookup)
+    assert cleaning.level['LST', 1] == 10.0
+
+    # give a custom HierarchicalLookup
+    cleaning_levels = HierarchicalLookup(
+        item=Float(10.0),
+        hierarchy=('type', 'id'),
+        configuration=[
+            ('type', 'LST', 5.0),
+            ('type', 'MST', 15.0),
+            ('id', 5, 20.0),
+        ]
+    )
+    cleaning = Cleaning(level=cleaning_levels)
+    assert isinstance(cleaning.level, HierarchicalLookup)
+    assert cleaning.level['LST', 1] == 5.0
+    assert cleaning.level['MST', 2] == 15.0
+    assert cleaning.level['LST', 5] == 20.0
+    assert cleaning.level['SST', 5] == 20.0
+    assert cleaning.level['SST', 10] == 10.0
+
+
+def test_invalid():
+    from config import Configurable, Float, Int
+    from config.items.lookup import Lookup, HierarchicalLookup
+    from config.exceptions import ConfigError
+
+
+    class Cleaning(Configurable):
+        level = Lookup(item=Float(5.0, allow_none=False), hierarchy=('type', 'id'))
+
+    with pytest.raises(ConfigError):
+        Cleaning(level=None)
+
+    cleaning = Cleaning()
+
+    wrong_item = HierarchicalLookup(item=Int(5), hierarchy=('type', 'id'))
+    with pytest.raises(ConfigError):
+        cleaning.level = wrong_item
+
+    wrong_hierarchy = HierarchicalLookup(item=Float(5.0), hierarchy=('foo', 'id'))
+    with pytest.raises(ConfigError):
+        cleaning.level = wrong_hierarchy
