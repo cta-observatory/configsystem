6653867e0a48492cd169516c578893166b909f23
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-01-29 14:05:29 +0100

    Update python versions ins setup

--- setup.py
+++ setup.py
@@ -14,13 +14,14 @@ extras_require["all"] = list({
 setup(
     name="config",
     packages=find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     extras_require=extras_require,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Development Status :: 3 - Alpha",
     ],
     zip_safe=False,
